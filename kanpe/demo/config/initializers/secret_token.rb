86d2117be3495950cdcc3b1f70f9c263da8f4437
ShoheiSUNAGAWA <kanpe777@gmail.com>
2012-08-25 18:25:42 +0900

    section6 taskA start
    Main: demo -> depot

---
+++ kanpe/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '3140635f873cb0fe144045f82a38cb2519c429c5e6609ba00d5e6a56b5f63f3190aafe4d9f34c592f72216ce80dcba34a6efa9ee65b630b1d26612d529b7d265'
---
+++ kanpe/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '869ca2041fabeaf58211f538804c5783ad2904d01d91c06dd9f725f32510bb5724a7354ac21c1b4ea33594896b7c8334cf0c6ac9b44efd4f10751ea242541522'
