d397ced482bae8ff393e948795b3274066c5c642
e125716@ie.u-ryukyu.ac.jp <e125716@gmail.com>
2012-08-27 16:46:24 +0900

    色々更新

---
+++ innparusu/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '95b5166fe3280b677e987d0bb02399e3b9d22419e546227e094c8457565165d6fcae8e4ea125f5a2dd4b9b710ccd877497a738839c02c10016bc5b27dc981c3b'
