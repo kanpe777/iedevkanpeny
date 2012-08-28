1a876a286ac83ff3fceef6a5e76114ee2712d7f2
innparusu <e125716@gmail.com>
2012-08-28 21:37:47 +0900

    test

--- innparusu/pokepirty/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pokepirty::Application.config.secret_token = '2f6583f15fe4728522a4ffbe809813b0b0d9f5a988af3a1c49102e770fe00af3ed53e1ffeb4ea0a52aab125f0739c1fb0e52fb4f4682913a9a816bdaff71411f'
