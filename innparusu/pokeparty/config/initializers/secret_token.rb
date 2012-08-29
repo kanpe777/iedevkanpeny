866f9b017cc3e91453e1f4bcb8f1fe312748af19
innparusu <e125716@gmail.com>
2012-08-29 11:27:24 +0900

    初期

---
+++ innparusu/pokeparty/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokeparty::Application.config.secret_token = 'fee34ece08898e04869591430a6748ae38f8343a1f7a2396e3c85ef4f5cbed33cc07534124efbf81e1b53f40d3c0c690c7e631a7fadc47e77badcaeaede62ef0'
