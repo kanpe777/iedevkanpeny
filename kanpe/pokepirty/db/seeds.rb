6d7c0540d70d43d9425288d3cc5432c8b06c0fe2
ShoheiSUNAGAWA <kanpe777@gmail.com>
2012-08-29 13:28:23 +0900

    add css, pokedbs's css is ok.

--- kanpe/pokepirty/db/seeds.rb
+++ kanpe/pokepirty/db/seeds.rb
@@ -22,3 +22,33 @@ Pokedb.create(number:          001,
               based:           65,
               bases:           45)
 
+Pokedb.create(number:          002,
+              image_url:       'poke/n2.gif',
+              name:            'フシギソウ',
+              type1:           'くさ',
+              type2:           'どく',
+              characteristic1: 'しんりょく',
+              characteristic2: 'ようりょくそ',
+              characteristic3: '',
+              baseh:           60,
+              basea:           62,
+              baseb:           63,
+              basec:           80,
+              based:           80,
+              bases:           60)
+
+Pokedb.create(number:          003,
+              image_url:       'poke/n3.gif',
+              name:            'フシギバナ',
+              type1:           'くさ',
+              type2:           'どく',
+              characteristic1: 'しんりょく',
+              characteristic2: 'ようりょくそ',
+              characteristic3: 'test',
+              baseh:           80,
+              basea:           82,
+              baseb:           83,
+              basec:           100,
+              based:           100,
+              bases:           80)
+
